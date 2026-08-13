849cea1b1b30be2540b651ca4ab1e3258257dc9e
shivanshu-samadhiya <shivanshusamadhiya23@gmail.com>
2026-08-13 23:33:06 +0530

    Remove frontend from repository

--- frontend/src/admin/services/paymentService.js
+++
@@ -1,8 +0,0 @@
-import axiosInstance from './axiosInstance';
-
-export const paymentService = {
-  getAllPayments: async () => {
-    const res = await axiosInstance.get('/payments');
-    return res.data;
-  }
-};
